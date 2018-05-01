2b6a43927d269b074f0954b96af18487b4f0b819
Cosmic Arrows <cosmicarrowsllc@Cosmics-MacBook-Pro.local>
2018-05-01 16:49:59 -0400

    Upload assets to Github from hardrive

---
+++ PlayingAroundLaurence/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PlayingAroundLaurence
+//
+//  Created by Laurence Wingo on 2/21/15.
+//  Copyright (c) 2015 Laurence Wingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PlayingAroundLaurence/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PlayingAroundLaurence
+//
+//  Created by Laurence Wingo on 2/21/15.
+//  Copyright (c) 2015 Laurence Wingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
